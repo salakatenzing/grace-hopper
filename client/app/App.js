917202a9e28db51381b875e3891f9661826e60c3
Andrew Jupina <andrewjupina8@gmail.com>
2023-04-19 19:09:21 -0700

    completed navbar, added promotion cards and product cards to home page but still need to dynamically change to be able to scroll

--- client/app/App.js
+++ client/app/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navbar from '../features/navbar/Navbar';
 import AppRoutes from './AppRoutes';
+import Home from '../features/home/Home';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <AppRoutes />
+      <AllProducts />
     </div>
   );
 };
--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -15,14 +15,64 @@ const Navbar = () => {
   return (
     <div>
       <h1>FS-App-Template</h1>
-      <nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
         {isLoggedIn ? (
-          <div>
+          <div className="container-fluid">
             {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <button type="button" onClick={logoutAndRedirectHome}>
-              Logout
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <div className="row w-100">
+                <div className="col-md-4">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center">
+                    <li className="nav-item">
+                      <Link to="/home">Home</Link>
+                    </li>
+                    <li className="nav-item dropdown">
+                      <button className="btn btn-body-tertiary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        Products
+                      </button>
+                      <ul className="dropdown-menu dropdown-menu-body-tertiary">
+                        <li className="dropdown-item">
+                          <Link to="/products/produce">Produce</Link>
+                        </li>
+                        <li className="dropdown-item">
+                          <Link to="/products/meat">Meat</Link>
+                        </li>
+                        <li className="dropdown-item">
+                          <Link to="/products/dairy&eggs">Dairy & Eggs</Link>
+                        </li>
+                        <li className="dropdown-item">
+                          <Link to="/products/beverages">Beverages</Link>
+                        </li>
+                        <li className="dropdown-item">
+                          <Link to="/products/driedgoods">Dried Goods</Link>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-md-4 d-flex justify-content-center">
+                  <form className="d-flex align-items-center search-form" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                  </form>
+                </div>
+                <div className="col-md-4 d-flex justify-content-end">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <Link to="/cart">Cart</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/login">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/signup">Sign up</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         ) : (
           <div>
@@ -32,7 +82,6 @@ const Navbar = () => {
           </div>
         )}
       </nav>
-      <hr />
     </div>
   );
 };
