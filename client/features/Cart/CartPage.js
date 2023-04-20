4274f7ec0519f13ac48f3551d7d7aee013a57a3a
Holden Lindblom <holden.lindblom@gmail.com>
2023-04-20 15:43:19 -0400

    merge issue fixes

--- client/features/Cart/CartPage.js
+++ client/features/Cart/CartPage.js
@@ -9,6 +9,7 @@ export default function CartPage() {
     <div className="row g-5 w-50 m-auto">
       <div className="col-md-5 col-lg-4 order-md-last">
         <h4 className="d-flex justify-content-between align-items-center mb-3">
+          <span className="text-primary">Your cart</span>
           <span className="badge bg-primary rounded-pill">3</span>
         </h4>
         <ul className="list-group mb-3">
