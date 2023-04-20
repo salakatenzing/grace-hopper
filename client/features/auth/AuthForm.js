ac347ac5cf88c07a745948352a677fbf42e05a23
Tenzing Salaka <salakatenzing@gmail.com>
2023-04-20 16:17:57 -0400

    fixed login bug and grabbed JWT

--- client/features/auth/AuthForm.js
+++ client/features/auth/AuthForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { authenticate } from '../../app/store';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 /**
   The AuthForm component can be used htmlFor Login or Sign Up.
@@ -12,6 +12,7 @@ import { Link } from 'react-router-dom';
 const AuthForm = ({ name, displayName }) => {
   const { error } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
@@ -19,6 +20,7 @@ const AuthForm = ({ name, displayName }) => {
     const email = evt.target.email.value;
     const password = evt.target.password.value;
     dispatch(authenticate({ email, password, method: formName }));
+    navigate('/', { replace: true });
   };
   const handleNoAccount = () => {
     if (name === 'login') {
