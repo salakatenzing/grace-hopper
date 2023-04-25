845f304411ffcd48f005a21f1cb80284aed0a3fc
Holden Lindblom <holden.lindblom@gmail.com>
2023-04-25 14:07:09 -0400

    remote db added

--- server/db/db.js
+++ server/db/db.js
@@ -1,28 +1,29 @@
 const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 
-const databaseName =
-    pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+// const databaseName =
+//   pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
 const config = {
-    logging: false,
+  port: '5432',
+  dialect: 'postgres',
+  dialectOptions: {
+    connectionString: process.env.DATABASE_URL,
+  },
 };
 
-if (process.env.LOGGING === 'true') {
-    delete config.logging;
-}
+// if (process.env.LOGGING === 'true') {
+//   delete config.logging;
+// }
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if (process.env.DATABASE_URL) {
-    config.dialectOptions = {
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    };
-}
+// if (process.env.DATABASE_URL) {
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
 
-const db = new Sequelize(
-    process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-    config
-);
+const db = new Sequelize(config);
 module.exports = db;
